02eb83805e14e80f4e130b990914137aa393eb0f
Marcos Sabatini <marcossabatini7@gmail.com>
2022-10-27 21:00:58 -0300

    impl product entity

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,10 @@
-export class Product {}
+export class Product {
+  id: string
+  name: string
+  price: number
+  quantity: number
+
+  constructor(init?: Partial<Product>) {
+    Object.assign(this, init)
+  }
+}
